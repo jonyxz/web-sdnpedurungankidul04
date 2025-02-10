3f9192a11f2389ab4cbed2aca5fb09c56415b192
Fachruddin <poenyoex@gmail.com>
2025-02-10 11:42:52 +0700

    Remove BeritaDetailPage component and view; update news page with new image and content

--- resources/views/livewire/news-page.blade.php
+++ resources/views/livewire/news-page.blade.php
@@ -40,14 +40,14 @@
                     <div class="col-lg-4 mb-4 mb-lg-0 pb-3 justify-center">
                         <div class="bg-white rounded-lg shadow-sm">
                             <a class="mb-2" href="#">
-                                <span class="img-fluid w-100 h-auto"><img src="{{ asset('assets/images/post_3.jpg') }}" alt="Image"></span>
+                                <span class="img-fluid w-100 h-auto"><img src="{{ asset('assets/images/sdn/berita1.jpeg') }}" alt="Image"></span>
                             </a>
                             <div class="px-4 py-4">
                                 <a href="#">
-                                    <h5 class="mb-2 font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
+                                    <h5 class="mb-2 font-bold tracking-tight text-gray-900">Pelatihan Batik Jumputan Kelas IV</h5>
                                 </a>
-                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">PPG Prajabatan merupakan program yang menghasilkan guru inovatif dan profesional di Indonesia. Program PPG prajabatan tahun 2023 berlangsung selama dua semester. Salah satu mata kuliah di PPG Prajabatan yaitu projek kepemimpinan.</p>
+                                <a href="https://www.kompasiana.com/linda12246/66003dd714709323d04d8fb2/pelatihan-batik-jumputan-siswa-kelas-iv-sdn-pedurungan-kidul-04-semarang" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Read more
                                 </a>
                             </div>
@@ -63,7 +63,7 @@
                                     <h5 class="mb-2 font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
                                 </a>
                                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <a href="/news/2" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Read more
                                 </a>
                             </div>
