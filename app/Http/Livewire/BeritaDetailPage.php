3318ae40157c3aeb5ea4b34d5f9fab1c98b4befa
Fachruddin <poenyoex@gmail.com>
2025-02-10 10:07:19 +0700

    Remove VideoPage component and associated view; add BeritaDetailPage component and view

--- app/Http/Livewire/BeritaDetailPage.php
+++ app/Http/Livewire/BeritaDetailPage.php
@@ -4,10 +4,10 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
-class VideoPage extends Component
+class BeritaDetailPage extends Component
 {
     public function render()
     {
-        return view('livewire.video-page');
+        return view('livewire.berita-detail-page');
     }
 }
