c42fdf55a813916653c8b92fc5eb6af5cb39afc0
jonyxz <johanridho08@gmail.com>
2025-02-18 11:05:30 +0700

    Add custom font support and improve text styling in homepage and welcome view

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -59,10 +59,10 @@
                         <img src="{{ asset('assets/images/sdn/profile_sd.png') }}" alt="Image" class="img-fluid w-80 h-80">
                     </div>
                     <div class="col-md-6 ml-auto pl-md-0">
-                        <span class="text-cursive h5 text-black">Kepala SDN Pedurungan Kidul 04</span><br>
-                        <span class="text-cursive h4 text-red">HIRNOWO, S.Pd</span>
+                        <span class="font-education h5 text-black">Kepala SDN Pedurungan Kidul 04</span><br>
+                        <span class="font-education h4 text-red">HIRNOWO, S.Pd</span>
                         <h3 class="text-black">Sambutan Kepala Sekolah</h3>
-                        <p>
+                        <p class="text-justify">
                             <span>Assalamu'alaikum wr.wb.</span><br><br>
                             <span>Puji syukur kepada Allah SWT atas rahmat-Nya sehingga website SDN Pedurungan Kidul 04 dapat terbit. Website ini bertujuan untuk memenuhi kebutuhan informasi dengan memanfaatkan teknologi. Kami berupaya menyediakan sarana yang kondusif bagi siswa, guru, orang tua, dan masyarakat dalam menghadapi perkembangan teknologi informasi yang pesat.</span><br>
                             <span>Tuntutan mutu pendidikan semakin tinggi, sehingga sekolah harus terus berinovasi agar tetap relevan di era globalisasi. Website sekolah menjadi sarana penting dalam komunikasi serta pusat informasi bagi siswa, guru, dan wali murid.</span><br>
@@ -72,25 +72,25 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div>        
 
         <!-- About -->
         <div class="site-section bg-gray-300">
             <div class="container">
                 <div class="row justify-content-center text-center section-2-title">
                     <div class="col-md-10">
-                        <span class="text-cursive h4 text-red">Sekilas Tentang</span>
+                        <span class="font-education h4 text-red">Sekilas Tentang</span>
                         <h3 class="text-black text-center font-bold">SDN PEDURUNGAN KIDUL 04</h3>
                         <div class="flex justify-center p-3 transform transition duration-500 ease-in-out hover:scale-105">
-                            <img src="{{ asset('assets/images/sdn/sekolah.jpg') }}" alt="Image" class="max-w-full h-auto">
+                            <img src="{{ asset('assets/images/sdn/sekolah.jpg') }}" alt="Image" class="max-w-full h-auto rounded-lg">
                         </div>
-                        <p class="mb-0">
+                        <p class="mb-0 p-4 text-justify">
                             Secara administrasi Sekolah Dasar (SD) Negeri Pedurungan Kidul 04 berada di Jalan Blancir Raya RT 06 RW 12 Kecamatan Pedurungan Kota Semarang. SD Negeri Pedurungan Kidul 04 terdiri dari beberapa bangunan utama, dengan rincian 8 (delapan) ruangan kelas, 1 (satu) ruangan Guru, 1 (satu) ruang Kepala Sekolah, 1 (satu) ruangan Perpustakaan, Mushola, Ruang UKS, halaman yang luas dan 1 (satu) kantin yang berada di dalam sekolah.
                         </p>
                     </div>
                 </div>
             </div>
-        </div>
+        </div>        
 
         <!-- Berita -->
         @include('livewire.news-page', ['hideHero' => true])
